f99f9f6e6c2ffb37160955809ea29440fcdc27a0
greybeard <odhiambohumphrey78@gmail.com>
2024-01-07 01:44:36 +0300

    sniffer

---
+++ python_networking/paramiko/socket_sniffer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import socket
+import os
+
+host = input("Enter host to listen on : ")
+
+def main():
+
+    if os.name == "nt":
+        socket_protocol = socket.IPPROTO_IP
+    else:
+        socket_protocol = socket.IPPROTO_ICMP
+
+    sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+
+    sniffer.bind((host, 0))
+
+    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+    
+    print(sniffer.recvfrom(65565))
+
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+
+
+if __name__ == "__main__":
+
+    main()
